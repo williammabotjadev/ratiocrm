6873e3ee467b65f27ba48a4c0d2d0635ca80d639
williammabotjadev <williammabotjadev@gmail.com>
2021-10-20 08:28:05 +0200

    nav circles, flex, method post form, home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,18 +34,24 @@
                         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur vero, tempore suscipit quod dignissimos quasi eos ea ratione architecto ipsam quos numquam doloremque veniam nihil rem provident eveniet explicabo quam in perferendis accusamus debitis. Necessitatibus suscipit reiciendis quisquam itaque! Eum repudiandae eveniet placeat maxime nemo magni similique aliquam molestias facere repellendus laborum numquam facilis laudantium autem optio illum odit, exercitationem vero sequi commodi minus sed ab dolore delectus! Optio quos quas eveniet mollitia dolorem rerum!
                     </p>
                     <br>
-                    <div class="d-flex flex-row">
+                    <div class="d-flex flex-row justify-content-around">
                         <a href="" class="btn btn-body">Find out More</a>
-                        <div class="d-flex flex-row">
+                        <div class="d-flex flex-row justify-content-between align-items-center">
                                 <span class="circle-nav">
-                                    &amp;
+                                    &#9711;
+                                </span>
+                                <span class="circle-nav">
+                                    &#9711;
+                                </span>
+                                <span class="circle-nav">
+                                    &#9711;
                                 </span>
                         </div>
                     </div>
                         
             </div>
             <div class="w-50 mt-lg-5 ml-lg-5 d-flex justify-content-center align-items-center">
-                    <form action="{{ route('register') }}" class="form-group w-75 home-form">
+                    <form action="{{ route('register') }}" method="POST" class="form-group w-75 home-form">
                         @csrf
                         <fieldset>
                         <legend class="form-title-text">Create an Account</legend>
