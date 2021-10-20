688b4caa43ec828d8643d543d779af57bd66ec94
williammabotjadev <williammabotjadev@gmail.com>
2021-10-20 08:48:47 +0200

    Register Controller, create function

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,8 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'firstname' => $data['firstname'],
+            'lastname' => $data['lasstname'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
