08fe60263afa8b5a171f7d74751e879dd3c2c6d4
williammabotjadev <williammabotjadev@gmail.com>
2021-10-21 14:36:18 +0200

    Company Model

---
+++ app/Models/Company.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'registration_no',
+        'street_address',
+        'surburb',
+        'citytown',
+        'zip_postal_code',
+        'state_province_region',
+        'email_address',
+        'contact_no',
+        'additional_contact_no',
+        'website',
+        'facebook_url',
+        'linkedin_url',
+        'twitter_url',
+        'contact_person_firstname',
+        'contact_person_lastname',
+        'contact_person_contact_no',
+        'contact_person_linkedin',
+        'industry',
+        'business_type'
+    ];
+}
