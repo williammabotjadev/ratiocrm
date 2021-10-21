d42088c13f9f572e021531407ab08c671206db59
williammabotjadev <williammabotjadev@gmail.com>
2021-10-21 15:00:58 +0200

    Employee Model

---
+++ app/Models/Employee.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'contact_no',
+        'email',
+        'street_address',
+        'surburb',
+        'citytown',
+        'zip_postal_code',
+        'state_province_region',
+        'linkedin_url',
+        'twitter_url',
+        'facebook_url'
+    ];
+
+    public function company()
+    {
+
+    }
+
+    public function customers()
+    {
+        
+    }
+}
