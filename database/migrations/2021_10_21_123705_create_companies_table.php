aa6f1a6afff5162dc4c1acd8e3a36f70842a29a6
williammabotjadev <williammabotjadev@gmail.com>
2021-10-21 14:54:53 +0200

    Company Migration

---
+++ database/migrations/2021_10_21_123705_create_companies_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->increment('id');
+            $table->string('name');
+            $table->string('registration_no')->nullable();
+            $table->text('street_address');
+            $table->string('surburb');
+            $table->string('citytown');
+            $table->string('zip_postal_code');
+            $table->string('state_province_region');
+            $table->string('email_address');
+            $table->string('contact_no');
+            $table->string('additional_contact_no')->nullable();
+            $table->string('website_url')->nullable();
+            $table->string('facebook_url')->nullable();
+            $table->string('linkedin_url')->nullable();
+            $table->string('twitter_url')->nullable();
+            $table->string('contact_person_firstname')->nullable();
+            $table->string('contact_person_lastname')->nullable();
+            $table->string('contact_person_contact_no')->nullable();
+            $table->string('contact_person_linkedin')->nullable();
+            $table->string('industry')->nullable();
+            $table->string('business_type')->nullable();
+            $table->string('customer_id')->nullable();
+            $table->foreign('id')->on('customers')->reference('id');
+            $table->string('employee_id')->nullable();
+            $table->foreign('id')->on('employees')->reference('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            //
+        });
+    }
+}
