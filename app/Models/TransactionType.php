4106bb138663261f9ed93315b36bfea453ce798c
williammabotjadev <williammabotjadev@gmail.com>
2021-10-21 19:21:21 +0200

    Transaction Type, Migration files

---
+++ app/Models/TransactionType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionType extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description'
+    ];
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class, 'transaction_id');
+    }
+
+    public function employee()
+    {
+        return $this->hasMany(Employee::class, 'employee_id');
+    }
+}
