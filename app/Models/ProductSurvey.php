98e8374d1e32a9eea7f76056683e3081b4df583b
williammabotjadev <williammabotjadev@gmail.com>
2021-10-22 14:23:37 +0200

    Edit ProductSurvey, ServiceSurvey Models

--- app/Models/ProductSurvey.php
+++ app/Models/ProductSurvey.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class ProductSurvey extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class, 'question_id');
+    }
 }
--- app/Models/ServiceSurvey.php
+++ app/Models/ServiceSurvey.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class ServiceSurvey extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description'
+    ];
+
+    public function service()
+    {
+        return $this->belongsTo(Product::class, 'service_id');
+    }
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class, 'question_id');
+    }
 }
