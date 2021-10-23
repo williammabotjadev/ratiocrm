6678899c2c6d64ce5e1cda350b8813d42679fc4b
williammabotjadev <williammabotjadev@gmail.com>
2021-10-23 11:13:41 +0200

    email_address, email bug fix, models, migrations

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -18,7 +18,7 @@ class Customer extends Model
         'zip_postal_code',
         'state_province_region',
         'country',
-        'email_address',
+        'email',
         'contact_no',
         'additional_contact_no',
         'website_url',
