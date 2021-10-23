f5669b4278635bcbedc98370d50accddff4ba303
williammabotjadev <williammabotjadev@gmail.com>
2021-10-23 11:28:48 +0200

    Business Controller NS, businessess

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Company;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $businesses = Company::all();
+        return view('home', ['businesses' => $businesses]);
     }
 }
