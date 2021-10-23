3937c9ad6cf6fb9aa4a8cddd40fbac38b03bf3a0
williammabotjadev <williammabotjadev@gmail.com>
2021-10-23 09:03:18 +0200

    Setup Your Business CTA, Home Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,12 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+        <div class="col-md-8 d-flex flex-column justify-content-center align-items-center">
+            <h1>Welcome to RatioCRM!</h1>
+            <br>
+            <h2>Setup your Business!</h2>
+            <br>
+            <button class="btn">Get Started</button>
         </div>
     </div>
 </div>
