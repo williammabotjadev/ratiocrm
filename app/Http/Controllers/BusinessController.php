b10ecd513c5b08dbd79e66e9990779f1e45512eb
williammabotjadev <williammabotjadev@gmail.com>
2021-10-23 11:24:01 +0200

    request data, business controller

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -14,7 +14,7 @@ class BusinessController extends Controller
 
     public function store(Request $request)
     {
-        $validated = $request->validate([
+        /*$validated = $request->validate([
             'name' => $request['businessname'],
             'registration_no' => $request['regno'],
             'street_address' => $request['streetaddress'],
@@ -29,23 +29,23 @@ class BusinessController extends Controller
             'contact_person_firstname' => $request['contactfname'],
             'contact_person_lastname' => $request['contactlname'],
             'contact_email' => $request['contactemal']
-        ]);
+        ]);*/
 
         $business_data = [
-            'name' => $validated['name'],
-            'registration_no' => $validated['registration_no'],
-            'street_address' => $validated['street_address'],
-            'surburb' => $validated['surburb'],
-            'citytown' => $validated['citytown'],
-            'zip_postal_code' => $validated['zip_postal_code'],
-            'state_province_region' => $validated['state_province_region'],
-            'country' => $validated['country'],
-            'email' => $validated['email_address'],
-            'contact_no' => $validated['contact_no'],
-            'website_url' => $validated['website_url'],
-            'contact_person_firstname' => $validated['contact_person_firstname'],
-            'contact_person_lastname' => $validated['contact_person_lastname'],
-            'contact_email' => $validated['contact_email']
+            'name' => $request['businessname'],
+            'registration_no' => $request['regno'],
+            'street_address' => $request['streetaddress'],
+            'surburb' => $request['surburb'],
+            'citytown' => $request['citytown'],
+            'zip_postal_code' => $request['zippostalcode'],
+            'state_province_region' => $request['stateprovinceregion'],
+            'country' => $request['country'],
+            'email' => $request['email'],
+            'contact_no' => $request['contactno'],
+            'website_url' => $request['website'],
+            'contact_person_firstname' => $request['contactfname'],
+            'contact_person_lastname' => $request['contactlname'],
+            'contact_email' => $request['contactemal']
         ];
 
         $business = Company::create($business_data);
