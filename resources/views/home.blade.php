75166e9e8f556e6416f5332c1c5e43fb413b8019
williammabotjadev <williammabotjadev@gmail.com>
2021-10-23 09:09:45 +0200

    home page, text blue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,13 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8 d-flex flex-column justify-content-center align-items-center">
-            <h1>Welcome to RatioCRM!</h1>
             <br>
-            <h2>Setup your Business!</h2>
             <br>
-            <button class="btn">Get Started</button>
+            <h1 class="blue-text">Welcome to RatioCRM!</h1>
+            <br>
+            <h2>Setup your first Business!</h2>
+            <br>
+            <a href="" class="btn btn-home btn-lg">Get Started</a>
         </div>
     </div>
 </div>
