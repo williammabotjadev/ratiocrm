1381f3595811391fb54a120757c6cbfd4e899bd3
williammabotjadev <williammabotjadev@gmail.com>
2021-10-23 10:47:09 +0200

    Catch Request Vars

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Company;
 
 class BusinessController extends Controller
 {
     public function setup()
     {
         return view('business.setup');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => $request['businessname'],
+            'registration_no' => $request['regno'],
+            'street_address' => $request['streetaddress'],
+            'surburb' => $request['surburb'],
+            'citytown' => $request['citytown'],
+            'zip_postal_code' => $request['zippostalcode'],
+            'state_province_region' => $request['stateprovinceregion'],
+            'country' => $request['country'],
+            'email_address' => $request['email'],
+            'contact_no' => $request['contactno'],
+            'website_url' => $request['website'],
+            'contact_person_firstname' => $request['contactfname'],
+            'contact_person_lastname' => $request['contactlname'],
+            'contact_email' => $request['contactemal']
+        ]);
+
+        $business = Company::create($validated);
+
+        
+        
     }
 }
