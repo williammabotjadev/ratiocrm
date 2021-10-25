99595f4cf546f509938a0f282961779e862fe066
williammabotjadev <williammabotjadev@gmail.com>
2021-10-25 14:58:13 +0200

    User->companies(), user_id ref

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -16,7 +16,7 @@ class BusinessController extends Controller
     {
         $user = $request->user();
         
-        $validated = $request->validate([
+        /*$validated = $request->validate([
             'name' => 'required|max:100',
             'registration_no' => 'required|max:100',
             'street_address' => 'required|max:255',
@@ -25,7 +25,7 @@ class BusinessController extends Controller
             'zip_postal_code' => 'required|max:50',
             'state_province_region' => 'required|max:100',
             'country' => 'required|max:100',
-            'email' => 'required|unique|max:100',
+            'email' => 'required|unique:companies|max:100',
             'contact_no' => 'required|max:50',
             'website_url' => 'required|max:100',
             'contact_person_firstname' => 'required|max:100',
@@ -33,26 +33,30 @@ class BusinessController extends Controller
             'contact_email' => 'required|max:100',
         ]);
 
+        dd($validated);*/
+
         $business_data = [
-            'name' => $validated['businessname'],
-            'registration_no' => $validated['regno'],
-            'street_address' => $validated['streetaddress'],
-            'surburb' => $validated['surburb'],
-            'citytown' => $validated['citytown'],
-            'zip_postal_code' => $validated['zippostalcode'],
-            'state_province_region' => $validated['stateprovinceregion'],
-            'country' => $validated['country'],
-            'email' => $validated['email'],
-            'contact_no' => $validated['contactno'],
-            'website_url' => $validated['website'],
-            'contact_person_firstname' => $validated['contactfname'],
-            'contact_person_lastname' => $validated['contactlname'],
-            'contact_email' => $validated['contactemal']
+            'name' => $request['businessname'],
+            'registration_no' => $request['regno'],
+            'street_address' => $request['streetaddress'],
+            'surburb' => $request['surburb'],
+            'citytown' => $request['citytown'],
+            'zip_postal_code' => $request['zippostalcode'],
+            'state_province_region' => $request['stateprovinceregion'],
+            'country' => $request['country'],
+            'email' => $request['email'],
+            'contact_no' => $request['contactno'],
+            'website_url' => $request['website'],
+            'contact_person_firstname' => $request['contactfname'],
+            'contact_person_lastname' => $request['contactlname'],
+            'contact_email' => $request['contactemal']
         ];
 
         $business = Company::create($business_data);
 
         $user->companies()->save($business);
+
+        dd($user);
 
         return view('home');
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,6 @@ class User extends Authenticatable
 
     public function companies()
     {
-        return $this->hasMany(Company::class, 'company_id');
+        return $this->hasMany(Company::class, 'user_id');
     }
 }
