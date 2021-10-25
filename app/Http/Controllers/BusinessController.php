f61c19f948827bafdea453295bfccd9a4ea41712
williammabotjadev <williammabotjadev@gmail.com>
2021-10-25 14:44:15 +0200

    Save Companies to User

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -14,43 +14,45 @@ class BusinessController extends Controller
 
     public function store(Request $request)
     {
-        /*$validated = $request->validate([
-            'name' => $request['businessname'],
-            'registration_no' => $request['regno'],
-            'street_address' => $request['streetaddress'],
-            'surburb' => $request['surburb'],
-            'citytown' => $request['citytown'],
-            'zip_postal_code' => $request['zippostalcode'],
-            'state_province_region' => $request['stateprovinceregion'],
-            'country' => $request['country'],
-            'email_address' => $request['email'],
-            'contact_no' => $request['contactno'],
-            'website_url' => $request['website'],
-            'contact_person_firstname' => $request['contactfname'],
-            'contact_person_lastname' => $request['contactlname'],
-            'contact_email' => $request['contactemal']
-        ]);*/
+        $user = $request->user();
+        
+        $validated = $request->validate([
+            'name' => 'required|max:100',
+            'registration_no' => 'required|max:100',
+            'street_address' => 'required|max:255',
+            'surburb' => 'required|max:100',
+            'citytown' => 'required|max:100',
+            'zip_postal_code' => 'required|max:50',
+            'state_province_region' => 'required|max:100',
+            'country' => 'required|max:100',
+            'email' => 'required|unique|max:100',
+            'contact_no' => 'required|max:50',
+            'website_url' => 'required|max:100',
+            'contact_person_firstname' => 'required|max:100',
+            'contact_person_lastname' => 'required|max:100',
+            'contact_email' => 'required|max:100',
+        ]);
 
         $business_data = [
-            'name' => $request['businessname'],
-            'registration_no' => $request['regno'],
-            'street_address' => $request['streetaddress'],
-            'surburb' => $request['surburb'],
-            'citytown' => $request['citytown'],
-            'zip_postal_code' => $request['zippostalcode'],
-            'state_province_region' => $request['stateprovinceregion'],
-            'country' => $request['country'],
-            'email' => $request['email'],
-            'contact_no' => $request['contactno'],
-            'website_url' => $request['website'],
-            'contact_person_firstname' => $request['contactfname'],
-            'contact_person_lastname' => $request['contactlname'],
-            'contact_email' => $request['contactemal']
+            'name' => $validated['businessname'],
+            'registration_no' => $validated['regno'],
+            'street_address' => $validated['streetaddress'],
+            'surburb' => $validated['surburb'],
+            'citytown' => $validated['citytown'],
+            'zip_postal_code' => $validated['zippostalcode'],
+            'state_province_region' => $validated['stateprovinceregion'],
+            'country' => $validated['country'],
+            'email' => $validated['email'],
+            'contact_no' => $validated['contactno'],
+            'website_url' => $validated['website'],
+            'contact_person_firstname' => $validated['contactfname'],
+            'contact_person_lastname' => $validated['contactlname'],
+            'contact_email' => $validated['contactemal']
         ];
 
         $business = Company::create($business_data);
 
-        $business->save();
+        $user->companies()->save($business);
 
         return view('home');
 
