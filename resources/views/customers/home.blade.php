a643901fcadeaef50c40ddec105ae7d3b48b4df9
williammabotjadev <williammabotjadev@gmail.com>
2021-10-28 10:56:31 +0200

    Customers page

--- resources/views/customers/home.blade.php
+++ resources/views/customers/home.blade.php
@@ -6,10 +6,15 @@
         <div class="d-flex flex-column justify-content-center align-items-center">
             <div class="d-flex flex-column justify-content-center align-items-center">
                 <h1>{{ $biz->name }}</h1>
-                <h5 class="blue-text">Overview</h5>
+                <h5 class="blue-text">Customers</h5>
                 <br>
             </div>
+        @if (count($customers) < 0)
             {{ $customers }}
+        @else 
+            <h3 class="blue-text">No Customer Data Yet</h3>
+        @endif
+        <br>
         </div>
         <a href="{{ route('biz', ['id' => $biz->id]) }}" class="btn btn-home">Business Home</a>
     </div>
