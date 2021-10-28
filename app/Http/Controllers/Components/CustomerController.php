c55b9eb3a5a7cea9bbacd699c5634547c735c0d6
williammabotjadev <williammabotjadev@gmail.com>
2021-10-28 16:05:15 +0200

    customer controller new, store functions

--- app/Http/Controllers/Components/CustomerController.php
+++ app/Http/Controllers/Components/CustomerController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\Components;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Company;
 
 class CustomerController extends Controller
 {
-    //
+    public function new_customer()
+    {
+        return view('customers.new');
+    }
+
+    public function store_customer()
+    {
+        return view('customers.new');
+    }
 }
