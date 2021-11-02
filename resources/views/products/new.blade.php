d94a3a0af5d9c7cf0c094788b5206b57b268c155
williammabotjadev <williammabotjadev@gmail.com>
2021-11-02 08:50:55 +0200

    products, new view

--- resources/views/products/new.blade.php
+++ resources/views/products/new.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="d-flex flex-row justify-content-center align-items-center">
-           <form action="{{ route('store-biz') }}" class="form-group" method="POST">
+           <form action="{{ route('store-product', ['id' => $biz->id]) }}" class="form-group" method="POST">
                @csrf
                <fieldset>
                    <legend>New Product</legend>
@@ -12,66 +12,32 @@
                    <br>
             <div class="d-flex flex-row">
             <div class="w-50 container">
-               <label for="businessname" class="form-label">Business Name</label>
+               <label for="prodname" class="form-label">Product Name</label>
                <br>
-               <input type="text" name="businessname" id="businessname" class="form-control" required>
+               <input type="text" name="prodname" id="prodname" class="form-control" required>
                <br>
-               <label for="regno" class="form-label">Registration Number</label>
+               <label for="prodsku" class="form-label">SKU Code</label>
                <br>
-               <input type="text" name="regno" id="regno" class="form-control" required>
+               <input type="text" name="prodsku" id="regno" class="form-control">
                <br>
-               <label for="streetaddress" class="form-label">Street Address</label>
+               <label for="description" class="form-label">Product Description</label>
                <br>
-               <input type="text" name="streetaddress" id="streetaddress" class="form-control" required>
+               <textarea cols="4" rows="5" name="description" id="description" class="form-control" required>
                <br>
-               <label for="surburb" class="form-label">Surburb</label>
+               <label for="producttype" class="form-label">Product Type</label>
                <br>
-               <input type="text" name="surburb" id="surburb" class="form-control" required>
+               <input type="text" name="producttype" id="surburb" class="form-control">
                <br>
-               <label for="citytown" class="form-label">City/Town</label>
+               <label for="costprice" class="form-label">Cost Price</label>
                <br>
-               <input type="text" name="citytown" id="citytown" class="form-control px-5" required>
+               <input type="number" name="costprice" id="costprice" class="form-control px-5" required>
                <br>
-            </div>
-            <div class="w-50 container">
-               <label for="zippostalcode" class="form-label">Zip/Postal Code</label>
+               <label for="sellingprice" class="form-label">Selling Price</label>
                <br>
-               <input type="text" name="zippostalcode" id="zippostalcode" class="form-control" required>
-               <br>
-               <label for="stateprovinceregion" class="form-label">State/Province/Region</label>
-               <br>
-               <input type="text" name="stateprovinceregion" id="stateprovinceregion" class="form-control" required>
-               <br>
-               <label for="country" class="form-label">Country</label>
-               <br>
-               <input type="text" name="country" id="country" class="form-control" required>
-               <br>
-               <label for="email" class="form-label">Email Address</label>
-               <br>
-               <input type="text" name="email" id="email" class="form-control" required>
-               <br>
-               <label for="contactno" class="form-label">Contact Number</label>
-               <br>
-               <input type="text" name="contactno" id="contactno" class="form-control px-5" required>
+               <input type="number" name="sellingprice" id="sellingprice" class="form-control px-5" required>
                <br>
             </div>
-            <div class="w-50 container">
-               <label for="website" class="form-label">Company Website</label>
-               <br>
-               <input type="text" name="website" id="website" class="form-control" required>
-               <br>
-               <label for="contactfname" class="form-label">Contact First Name</label>
-               <br>
-               <input type="text" name="contactfname" id="contactfname" class="form-control" required>
-               <br>
-               <label for="contactlname" class="form-label">Contact Last Name</label>
-               <br>
-               <input type="text" name="contactlname" id="contactlname" class="form-control" required>
-               <br>
-               <label for="contactemail" class="form-label">Contact Email</label>
-               <br>
-               <input type="text" name="contactemail" id="contactemail" class="form-control" required>
-               <br>
+            
                <br>
                <div class="d-flex flex-row mt-2">
                     <button type="submit" class="btn btn-home px-5">Create</button>
@@ -79,7 +45,7 @@
                </div>
               
                </div>
-            </div>   
+  
             </fieldset>
            </form>
         </div>
